bbf6ffd82ecb24271d75ac19658d313d309542c7
Mikhail-Ivanov <mikhail.ivanov8@gmail.com>
2019-10-18 15:48:12 +0400

    Add sample table owner data to quick start (#139)

--- databuilder/models/table_owner.py
+++ databuilder/models/table_owner.py
@@ -19,14 +19,14 @@ class TableOwner(Neo4jCsvSerializable):
                  db_name,  # type: str
                  schema_name,  # type: str
                  table_name,  # type: str
-                 owners,  # type: List
+                 owners,  # type: str
                  cluster='gold',  # type: str
                  ):
         # type: (...) -> None
         self.db = db_name.lower()
         self.schema = schema_name.lower()
         self.table = table_name.lower()
-        self.owners = owners
+        self.owners = [owner.strip() for owner in owners.lower().split(',')]
 
         self.cluster = cluster.lower()
         self._node_iter = iter(self.create_nodes())
