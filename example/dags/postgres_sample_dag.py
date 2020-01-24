3c9018d4bb780111657e977d3047a67ed4300bf7
unknown <darensin01@gmail.com>
2020-01-24 16:12:53 +0800

    Added new line at end of file

--- example/dags/postgres_sample_dag.py
+++ example/dags/postgres_sample_dag.py
@@ -167,3 +167,4 @@ with DAG('amundsen_databuilder', default_args=default_args, **dag_args) as dag:
         task_id='create_es_publisher_sample_job',
         python_callable=create_es_publisher_sample_job
     )
+    
