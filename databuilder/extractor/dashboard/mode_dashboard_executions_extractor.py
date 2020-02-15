4f6e82a49356226089f2c7fd6d5a76747478c556
Jin Hyuk Chang <jinchang@lyft.com>
2020-02-14 16:16:28 -0800

    Transform ISO8601 to epoch

--- databuilder/extractor/dashboard/mode_dashboard_executions_extractor.py
+++ databuilder/extractor/dashboard/mode_dashboard_executions_extractor.py
@@ -6,9 +6,10 @@ from typing import Any  # noqa: F401
 
 from databuilder import Scoped
 from databuilder.extractor.base_extractor import Extractor
-from databuilder.extractor.restapi.rest_api_extractor import RestAPIExtractor, REST_API_QUERY, MODEL_CLASS
+from databuilder.extractor.restapi.rest_api_extractor import RestAPIExtractor, REST_API_QUERY
 from databuilder.rest_api.base_rest_api_query import RestApiQuerySeed
 from databuilder.rest_api.rest_api_query import RestApiQuery
+from databuilder.transformer.timestamp_string_to_epoch import TimestampStringToEpoch, FIELD_NAME
 
 # CONFIG KEYS
 ORGANIZATION = 'organization'
@@ -32,16 +33,21 @@ class ModeDashboardExecutionsExtractor(Extractor):
         self._extractor = RestAPIExtractor()
         rest_api_extractor_conf = Scoped.get_scoped_conf(conf, self._extractor.get_scope()).with_fallback(
             ConfigFactory.from_dict(
-                {REST_API_QUERY: restapi_query,}
+                {REST_API_QUERY: restapi_query, }
             )
         )
 
         self._extractor.init(conf=rest_api_extractor_conf)
+        self._timestamp_str_to_epoch_transformer = TimestampStringToEpoch()
+        self._timestamp_str_to_epoch_transformer.init(
+            conf=ConfigFactory.from_dict(
+                {FIELD_NAME: 'execution_timestamp', }
+            )
+        )
 
     def extract(self):
         # type: () -> Any
-
-        return self._extractor.extract()
+        return self._timestamp_str_to_epoch_transformer.transform(record= self._extractor.extract())
 
     def get_scope(self):
         # type: () -> str
@@ -75,7 +81,7 @@ class ModeDashboardExecutionsExtractor(Extractor):
         field_names = ['dashboard_id', 'last_run_resource_path']
         last_run_resource_path_query = RestApiQuery(query_to_join=spaces_query, url=url, params=params,
                                                     json_path=json_path, field_names=field_names, skip_no_result=True
-                                                    ,json_path_contains_or=True)
+                                                    , json_path_contains_or=True)
 
         url = 'https://app.mode.com{last_run_resource_path}'
         json_path = '[state,completed_at]'
