1a6754c827b5c6a28fd3ba085ab3fa02284c5a1d
christina stead <cstead@lyft.com>
2020-03-06 17:21:12 -0800

    update return signature

--- databuilder/models/table_badge.py
+++ databuilder/models/table_badge.py
@@ -4,6 +4,6 @@ class TableBadge(object):
     """
     Representation of badges
     """
-    def __init__(self, badge_name) -> None:
+    def __init__(self, badge_name):
         self.tag_name = badge_name
         self.tag_type = 'badge'
