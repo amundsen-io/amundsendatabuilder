2be621170bb6ecba15d4e4063b8e18514b893b9f
Andrew <andrjc4@vt.edu>
2020-07-09 13:57:17 -0400

    convert table source

--- databuilder/models/table_source.py
+++ databuilder/models/table_source.py
@@ -1,11 +1,10 @@
 from typing import Any, Dict, List, Union  # noqa: F401
 
-from databuilder.models.neo4j_csv_serde import Neo4jCsvSerializable, NODE_KEY, \
-    NODE_LABEL, RELATION_START_KEY, RELATION_START_LABEL, RELATION_END_KEY, \
-    RELATION_END_LABEL, RELATION_TYPE, RELATION_REVERSE_TYPE
+from databuilder.models.neo4j_csv_serde import Neo4jCsvSerializable
 
 from databuilder.models.table_metadata import TableMetadata
-
+from databuilder.models.graph_node import GraphNode
+from databuilder.models.graph_relationship import GraphRelationship
 
 class TableSource(Neo4jCsvSerializable):
     # type: (...) -> None
@@ -38,7 +37,7 @@ class TableSource(Neo4jCsvSerializable):
         self._relation_iter = iter(self.create_relation())
 
     def create_next_node(self):
-        # type: (...) -> Union[Dict[str, Any], None]
+        # type: (...) -> Union[GraphNode, None]
         # return the string representation of the data
         try:
             return next(self._node_iter)
@@ -46,7 +45,7 @@ class TableSource(Neo4jCsvSerializable):
             return None
 
     def create_next_relation(self):
-        # type: (...) -> Union[Dict[str, Any], None]
+        # type: (...) -> Union[GraphRelationship, None]
         try:
             return next(self._relation_iter)
         except StopIteration:
@@ -67,33 +66,37 @@ class TableSource(Neo4jCsvSerializable):
                                                           table=self.table)
 
     def create_nodes(self):
-        # type: () -> List[Dict[str, Any]]
+        # type: () -> List[GraphNode]
         """
         Create a list of Neo4j node records
         :return:
         """
-        results = [{
-            NODE_KEY: self.get_source_model_key(),
-            NODE_LABEL: TableSource.LABEL,
-            'source': self.source,
-            'source_type': self.source_type
-        }]
+        node = GraphNode(
+            id=self.get_source_model_key(),
+            label=TableSource.LABEL,
+            node_attributes={
+                'source': self.source,
+                'source_type': self.source_type
+            }
+        )
+        results = [node]
         return results
 
     def create_relation(self):
-        # type: () -> List[Dict[str, Any]]
+        # type: () -> List[GraphRelationship]
         """
         Create a list of relation map between owner record with original hive table
         :return:
         """
-        results = [{
-            RELATION_START_KEY: self.get_source_model_key(),
-            RELATION_START_LABEL: TableSource.LABEL,
-            RELATION_END_KEY: self.get_metadata_model_key(),
-            RELATION_END_LABEL: TableMetadata.TABLE_NODE_LABEL,
-            RELATION_TYPE: TableSource.SOURCE_TABLE_RELATION_TYPE,
-            RELATION_REVERSE_TYPE: TableSource.TABLE_SOURCE_RELATION_TYPE
-        }]
+        relationship = GraphRelationship(
+            start_label=TableSource.LABEL,
+            start_key=self.get_source_model_key(),
+            end_label=TableMetadata.TABLE_NODE_LABEL,
+            end_key=self.get_metadata_model_key(),
+            type=TableSource.SOURCE_TABLE_RELATION_TYPE,
+            reverse_type=TableSource.TABLE_SOURCE_RELATION_TYPE
+        )
+        results = [relationship]
         return results
 
     def __repr__(self):
