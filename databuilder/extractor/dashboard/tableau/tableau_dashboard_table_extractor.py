2f018e963fc06fb2fe711cc066ebfe278a29f0a6
Carter Landis <carter.landis@gusto.com>
2020-07-20 16:10:58 -0500

    Fix linting errors in Tableau table extractor
    Signed-off-by: Carter Landis <carter.landis@gusto.com>

--- databuilder/extractor/dashboard/tableau/tableau_dashboard_table_extractor.py
+++ databuilder/extractor/dashboard/tableau/tableau_dashboard_table_extractor.py
@@ -1,28 +1,13 @@
 import logging
-import requests
-import uuid
 
 from pyhocon import ConfigTree, ConfigFactory  # noqa: F401
 from typing import Any  # noqa: F401
 
-from databuilder import Scoped
-
 from databuilder.extractor.base_extractor import Extractor
-from databuilder.extractor.dashboard.tableau.tableau_dashboard_utils import TableauDashboardUtils, TableauDashboardAuth
-
-from databuilder.extractor.dashboard.tableau.tableau_dashboard_constants import *
+from databuilder.extractor.dashboard.tableau.tableau_dashboard_utils import TableauDashboardAuth
 
 from databuilder.rest_api.rest_api_query import RestApiQuery  # noqa: F401
 from databuilder.rest_api.base_rest_api_query import BaseRestApiQuery  # noqa: F401
-from databuilder.rest_api.base_rest_api_query import EmptyRestApiQuerySeed
-from databuilder.rest_api.base_rest_api_query import RestApiQuerySeed
-from databuilder.rest_api.tableau.tableau_paginated_rest_api_query import TableauPaginatedRestApiQuery
-
-from databuilder.transformer.base_transformer import ChainedTransformer
-from databuilder.transformer.dict_to_model import DictToModel, MODEL_CLASS
-from databuilder.transformer.template_variable_substitution_transformer import \
-    TemplateVariableSubstitutionTransformer, TEMPLATE, FIELD_NAME as VAR_FIELD_NAME
-from databuilder.transformer.timestamp_string_to_epoch import TimestampStringToEpoch, FIELD_NAME
 
 LOGGER = logging.getLogger(__name__)
 
