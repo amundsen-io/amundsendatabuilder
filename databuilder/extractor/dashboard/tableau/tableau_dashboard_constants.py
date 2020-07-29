347225992410ed6b3322d5744ae07f2ce8e51ffd
Carter Landis <carter.landis@gusto.com>
2020-07-29 15:55:56 -0500

    Update Tableau extractors scopes
    Signed-off-by: Carter Landis <carter.landis@gusto.com>

--- databuilder/extractor/dashboard/tableau/tableau_dashboard_constants.py
+++ databuilder/extractor/dashboard/tableau/tableau_dashboard_constants.py
@@ -1,6 +1,8 @@
 API_VERSION = 'api_version'
 TABLEAU_HOST = 'tableau_host'
 SITE_NAME = 'site_name'
-TABLEAU_ACCESS_TOKEN_NAME = 'tableau_access_token_name'
-TABLEAU_ACCESS_TOKEN_SECRET = 'tableau_access_token_secret'
+TABLEAU_ACCESS_TOKEN_NAME = 'tableau_personal_access_token_name'
+TABLEAU_ACCESS_TOKEN_SECRET = 'tableau_personal_access_token_secret'
 EXCLUDED_PROJECTS = 'excluded_projects'
+EXTERNAL_CLUSTER_NAME = "external_cluster_name"
+EXTERNAL_DATABASE_NAME = "external_database_name"
