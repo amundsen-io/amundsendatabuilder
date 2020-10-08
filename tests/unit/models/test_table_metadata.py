dd85d2e18469f9e4486cedb0c1a5d35925d6ca01
Allison Suarez Miranda <asuarezmiranda@lyft.com>
2020-10-08 13:35:27 -0700

    pushing wahtever I have
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>

--- tests/unit/models/test_table_metadata.py
+++ tests/unit/models/test_table_metadata.py
@@ -108,6 +108,7 @@ class TestTableMetadata(unittest.TestCase):
         node_row = self.table_metadata.next_node()
         actual = []
         while node_row:
+            # print("NODE ROW: "+str(node_row))
             actual.append(node_row)
             node_row = self.table_metadata.next_node()
         print("ACTUAL")
