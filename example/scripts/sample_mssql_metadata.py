57b776ffb70a901a01da9a3c461a6861519dd44b
Joseph Atkins-Turkish <jatkins-turkish@brex.com>
2020-11-09 18:03:00 -0800

    Factor out large dict
    Signed-off-by: Joseph Atkins-Turkish <jatkins-turkish@brex.com>

--- example/scripts/sample_mssql_metadata.py
+++ example/scripts/sample_mssql_metadata.py
@@ -96,27 +96,24 @@ def run_mssql_job():
     node_files_folder = "{tmp_folder}/nodes/".format(tmp_folder=tmp_folder)
     relationship_files_folder = "{tmp_folder}/relationships/".format(tmp_folder=tmp_folder)
 
-    job_config = ConfigFactory.from_dict(
-        {
-            # MSSQL Loader
-            "extractor.mssql_metadata.{}".format(MSSQLMetadataExtractor.WHERE_CLAUSE_SUFFIX_KEY): where_clause_suffix,
-            "extractor.mssql_metadata.{}".format(MSSQLMetadataExtractor.USE_CATALOG_AS_CLUSTER_NAME): True,
-            "extractor.mssql_metadata.extractor.sqlalchemy.{}".format(
-                SQLAlchemyExtractor.CONN_STRING
-            ): connection_string(),
-            # NEO4J Loader
-            "loader.filesystem_csv_neo4j.{}".format(FsNeo4jCSVLoader.NODE_DIR_PATH): node_files_folder,
-            "loader.filesystem_csv_neo4j.{}".format(FsNeo4jCSVLoader.RELATION_DIR_PATH): relationship_files_folder,
-            "publisher.neo4j.{}".format(neo4j_csv_publisher.NODE_FILES_DIR): node_files_folder,
-            "publisher.neo4j.{}".format(neo4j_csv_publisher.RELATION_FILES_DIR): relationship_files_folder,
-            "publisher.neo4j.{}".format(neo4j_csv_publisher.NEO4J_END_POINT_KEY): neo4j_endpoint,
-            "publisher.neo4j.{}".format(neo4j_csv_publisher.NEO4J_USER): neo4j_user,
-            "publisher.neo4j.{}".format(neo4j_csv_publisher.NEO4J_PASSWORD): neo4j_password,
-            "publisher.neo4j.{}".format(
-                neo4j_csv_publisher.JOB_PUBLISH_TAG
-            ): "unique_tag",  # should use unique tag here like {ds}
-        }
-    )
+    config_dict = {
+        # MSSQL Loader
+        "extractor.mssql_metadata.{}".format(MSSQLMetadataExtractor.WHERE_CLAUSE_SUFFIX_KEY): where_clause_suffix,
+        "extractor.mssql_metadata.{}".format(MSSQLMetadataExtractor.USE_CATALOG_AS_CLUSTER_NAME): True,
+        "extractor.mssql_metadata.extractor.sqlalchemy.{}".format(SQLAlchemyExtractor.CONN_STRING): connection_string(),
+        # NEO4J Loader
+        "loader.filesystem_csv_neo4j.{}".format(FsNeo4jCSVLoader.NODE_DIR_PATH): node_files_folder,
+        "loader.filesystem_csv_neo4j.{}".format(FsNeo4jCSVLoader.RELATION_DIR_PATH): relationship_files_folder,
+        "publisher.neo4j.{}".format(neo4j_csv_publisher.NODE_FILES_DIR): node_files_folder,
+        "publisher.neo4j.{}".format(neo4j_csv_publisher.RELATION_FILES_DIR): relationship_files_folder,
+        "publisher.neo4j.{}".format(neo4j_csv_publisher.NEO4J_END_POINT_KEY): neo4j_endpoint,
+        "publisher.neo4j.{}".format(neo4j_csv_publisher.NEO4J_USER): neo4j_user,
+        "publisher.neo4j.{}".format(neo4j_csv_publisher.NEO4J_PASSWORD): neo4j_password,
+        "publisher.neo4j.{}".format(
+            neo4j_csv_publisher.JOB_PUBLISH_TAG
+        ): "unique_tag",  # should use unique tag here like {ds}
+    }
+    job_config = ConfigFactory.from_dict(config_dict)
 
     job = DefaultJob(
         conf=job_config,
